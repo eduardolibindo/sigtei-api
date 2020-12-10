e115a9e62c2370222e65e74445790b9fd00387bc
Eduardo <eduardo.libindo@itaqui.rs.gov.br>
2020-12-10 10:43:11 -0300

    new

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api-docs', require('_helpers/swagger'));
 app.use(errorHandler);
 
 app.use(express.static("dist/sigtei"));
-app.post("/", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "dist", "sigtei", "index.html"));
 });
 
