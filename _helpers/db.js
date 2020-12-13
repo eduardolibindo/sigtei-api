557a266e280cd2a38748a75d47d2342b5b7f5aad
eduardolibindo <57646249+eduardolibindo@users.noreply.github.com>
2020-12-13 03:14:33 -0300

    new

--- _helpers/db.js
+++ _helpers/db.js
@@ -13,16 +13,21 @@ async function initialize() {
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // conectar ao banco de dados
-    const sequelize = new Sequelize(database, user, password, { host:'klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', dialect: 'mysql' });
+    const sequelize = new Sequelize(database, user, password, { host:'l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', dialect: 'mysql' });
 
     // modelos de inicialização e adicioná-los ao objeto db exportado
     db.Account = require('../msc/accounts/account.model')(sequelize);
     db.RefreshToken = require('../msc/accounts/refresh-token.model')(sequelize);
-    db.Places = require('../msc/places/places.model')(sequelize);
+    db.Places = require('../msc/accounts/places.model')(sequelize);
 
     // define relacionamentos
     db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
     db.RefreshToken.belongsTo(db.Account);
+    
+    db.Places.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
+    db.RefreshToken.belongsTo(db.Places);
+
+    
     db.Places.belongsTo(db.Account);
     
     // sincroniza todos os modelos com o banco de dados
--- msc/places/places.service.js
+++ msc/places/places.service.js
@@ -1,5 +1,11 @@
+const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const crypto = require("crypto");
+const { Op } = require('sequelize');
+const sendEmail = require('_helpers/send-email');
 const db = require('_helpers/db');
-
+const Role = require('_helpers/role');
 
 module.exports = {
     // authenticate,
