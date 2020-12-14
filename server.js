18ac005fd299cb5b0326ffa46955803aac0a2b51
eduardolibindo <57646249+eduardolibindo@users.noreply.github.com>
2020-12-14 04:07:23 -0300

    new

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const errorHandler = require('_middleware/error-handler');
 
+app.use(cookieParser());
+
 // rotas api
 app.use('/accounts', require('./accounts/accounts.controller'));
 app.use('/places', require('./places/places.controller'));
@@ -13,12 +15,13 @@ app.use('/places', require('./places/places.controller'));
 // rotas docs swagger
 app.use('/api-docs', require('_helpers/swagger'));
 
-// permitir solicitações de cors de qualquer origem e com credenciais
-app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cookieParser());
+
+// permitir solicitações de cors de qualquer origem e com credenciais
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+
 
 // manipulador de erro global
 app.use(errorHandler);
