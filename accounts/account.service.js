e08b57514e9db5eb2b20ceec3c2049608b5d97dc
eduardolibindo <57646249+eduardolibindo@users.noreply.github.com>
2020-12-14 16:58:53 -0300

    new

--- accounts/account.service.js
+++ accounts/account.service.js
@@ -224,7 +224,7 @@ async function getAccount(id) {
 }
 
 async function getRefreshToken(token) {
-    const refreshToken = await db.RefreshToken.findOne({ where: { token } });
+    const refreshToken = await db.RefreshToken.findOne({ where: { token: 'token' } });
     if (!refreshToken || !refreshToken.isActive) throw 'Token inválido';
     return refreshToken;
 }
