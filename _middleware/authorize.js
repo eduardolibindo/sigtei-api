953d8cd7301e46ca5ab4c4bf6e1ff06fb16c7106
Eduardo <eduardo.libindo@itaqui.rs.gov.br>
2020-12-16 11:40:34 -0300

    new

--- _middleware/authorize.js
+++ _middleware/authorize.js
@@ -18,8 +18,8 @@ function authorize(roles = []) {
         // autorizar com base na função do usuário
         async (req, res, next) => {
             const account = await mongodb.Account.findOne({ _id: req.user.id }); // findById(req.user.id);
-            const refreshTokens = await mongodb.RefreshToken.find({ account: account.id });
-
+ const refreshTokens = await mongodb.RefreshToken.find({ account: account._id });
+           
             if (!account || (roles.length && !roles.includes(account.role))) {
                 // conta não existe mais ou função não autorizada
                 return res.status(401).json({ message: 'Não autorizado' });
