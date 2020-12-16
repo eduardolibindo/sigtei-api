ba46728c3f93ff1a72a9c03c948607460ca056c9
eduardolibindo <57646249+eduardolibindo@users.noreply.github.com>
2020-12-16 20:08:38 -0300

    new

--- _middleware/authorize.js
+++ _middleware/authorize.js
@@ -18,7 +18,7 @@ function authorize(roles = []) {
         // autorizar com base na função do usuário
         async (req, res, next) => {
             const account = await mongodb.Account.findById(req.user.id);
-            const refreshTokens = await mongodb.RefreshToken.find({ account: account.id });
+            const refreshTokens = await mongodb.RefreshToken.findById({ account: account.id });
 
             if (!account || (roles.length && !roles.includes(account.role))) {
                 // conta não existe mais ou função não autorizada
