57e00490175c24ab958742fd847d3345bf9344c4
Eduardo <eduardo.libindo@itaqui.rs.gov.br>
2020-12-16 11:10:22 -0300

    new

--- _middleware/authorize.js
+++ _middleware/authorize.js
@@ -17,7 +17,7 @@ function authorize(roles = []) {
 
         // autorizar com base na função do usuário
         async (req, res, next) => {
-            const account = await mongodb.Account.findById(req.user.id);
+            const account = await mongodb.Account.findOne({ _id: req.user.id }); // findById(req.user.id);
             const refreshTokens = await mongodb.RefreshToken.find({ account: account.id });
 
             if (!account || (roles.length && !roles.includes(account.role))) {
