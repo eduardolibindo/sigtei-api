dd64215eab53765db236aeb7a0b4c80b63429378
eduardolibindo <57646249+eduardolibindo@users.noreply.github.com>
2020-12-22 00:18:52 -0300

    new

--- _middleware/authorize.js
+++ _middleware/authorize.js
@@ -20,8 +20,8 @@ function authorize(roles = []) {
             // const account = await mongodb.Account.findOne({ _id: req.user.id });
             // const refreshTokens = await mongodb.RefreshToken.findOne({ account: account._id });
 
-            const account = await mongodb.Account.findById(req.user._id);
-            const refreshTokens = await mongodb.RefreshToken.find({ account: account._id });
+            const account = await mongodb.Account.findById(req.user.id);
+            const refreshTokens = await mongodb.RefreshToken.findOne({ account: account._id });
 
             if (!account || (roles.length && !roles.includes(account.role))) {
                 // conta não existe mais ou função não autorizada
