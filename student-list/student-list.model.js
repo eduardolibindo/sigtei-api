a649a476f5aba58129d24734772d1613ae7ab4e1
eduardolibindo <57646249+eduardolibindo@users.noreply.github.com>
2020-12-23 17:06:33 -0300

    new

--- student-list/student-list.model.js
+++ student-list/student-list.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    account: { type: Schema.Types.ObjectId, ref: 'Account' },
+    id: { type: String, required: true },
     title: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
