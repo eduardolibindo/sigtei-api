1d5a3cd544b39a53e2b65fe4b02e0629320a9441
eduardolibindo <57646249+eduardolibindo@users.noreply.github.com>
2020-12-26 02:44:46 -0300

    new

--- student-list/student-list.service.js
+++ student-list/student-list.service.js
@@ -22,7 +22,7 @@ async function getstudentListAll() {
 
 async function getaddressAll() {
     const studentLists = await db.StudentList.find();
-    return studentLists.map(studentLists => basicDetailsStudentList(address));
+    return studentLists.map(studentLists => basicDetailsStudentList(studentLists.address));
 }
 
 async function getstudentListById(id) {
