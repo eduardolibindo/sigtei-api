80a733e1592a0072a949549eb1f1cd7cdb899ba5
eduardolibindo <57646249+eduardolibindo@users.noreply.github.com>
2020-12-27 20:57:10 -0300

    new

--- student-list/student-list.service.js
+++ student-list/student-list.service.js
@@ -67,8 +67,8 @@ async function _deleteStudentList(id) {
 }
 
 async function _deleteListAll() {
-    const studentLists = await db.StudentList.deleteMany({});
-    return studentLists;
+    const studentLists = await db.StudentList.find();
+    return studentLists.remove();
 }
 
 async function getStudentList(id) {
